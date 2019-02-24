2e2f19574bb6de9b97f4df85e07d950bbc660d1e
anishhegde <anish.h3@gmail.com>
2019-02-24 11:52:39 -0600

    Added checkin api

--- app/api_v1/__init__.py
+++ app/api_v1/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from flask import jsonify
 
 
 api = Blueprint('api_v1', __name__)
@@ -7,6 +8,11 @@ api = Blueprint('api_v1', __name__)
 # Import any endpoints here to make them available
 #from . import dis_endpoint, dat_endpoint
 
-@api.route('/test')
+@api.route('/checkin')
 def hello_world():
-    return 'Hello, World!'
+    d = {
+        "lattitude" : -96.754,
+        "longitude" : 32.99,
+        "user_id" : 10001
+    }
+    return jsonify(d)
